cc6898d6a83f06d2df72a0d5e1d6ef41d01112a0
Chineze Ogugua <christabelogugua@gmail.com>
2023-01-14 01:54:00 -0800

    fix task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 # Creates a file
 file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
-=======
-# create a file in /tmp
-file { '/tmp/school':
-        ensure  => file,
-        mode    => '0744',
-        owner   => 'www-data',
-        group   => 'www-data',
-        content => 'I love Puppet'
->>>>>>> ba77720530a9b51c0950f3ee95931dbb80e36ee7
 }
